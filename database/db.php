ec72fb8569c50759dd3c0a6b89ddf1c0ca9bcfb6
literat <tomaslitera@hotmail.com>
2014-09-18 12:18:53 +0200

    Database example

---
+++ database/db.php
@@ -0,0 +1,32 @@
+<?php
+
+# loader Nix libraries
+require_once '../../src/Nix/loader.php';
+
+use Nix\Debugging\Debugger,
+	Nix\Database\Db;
+
+Debugger::init(true);
+
+Db::connect(array('database' => 'nix_examples'));
+
+echo '<h2>escaping</h2>';
+$a = array(
+	'test%r' => 'Now()',
+	'tests%s' => 'SuperTest\'',
+	'tsadst' => true
+);
+
+Debugger::dump(db::getConnection()->escapeArray($a));
+
+echo '<h2>fetch field</h2>';
+Debugger::dump(Db::fetchField("select [name] from [albums] order by RAND() limit 1"));
+
+echo '<h2>fetch all</h2>';
+Debugger::dump(Db::fetchAll("select * from [albums] order by [name] limit 3"));
+
+echo '<h2>fetch pairs</h2>';
+Debugger::dump(Db::fetchPairs("select [id], [name] from [albums] order by RAND() limit 10"));
+
+echo '<h2>fetch pairs</h2>';
+Debugger::dump(Db::fetchPairs("select [name] from [albums] order by RAND() limit 10"));
---
+++ database/nix_examples.sql
@@ -0,0 +1,53 @@
+/*
+Source Server         : localhost
+Source Server Version : 5620
+Source Host           : localhost:3306
+Source Database       : nix_examples
+
+Target Server Type    : MYSQL
+*/
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+-- ------------------------
+-- Database: `nix_examples`
+-- ------------------------
+CREATE DATABASE IF NOT EXISTS `nix_examples` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `nix_examples`;
+
+SET FOREIGN_KEY_CHECKS=0;
+-- ----------------------------
+-- Table structure for `albums`
+-- ----------------------------
+DROP TABLE IF EXISTS `albums`;
+CREATE TABLE `albums` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) COLLATE utf8_czech_ci NOT NULL,
+  `artist_id` int(100) NOT NULL,
+  `disk` enum('klasický','dvoj','troj','více') COLLATE utf8_czech_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+
+-- ----------------------------
+-- Records of albums
+-- ----------------------------
+INSERT INTO `albums` VALUES ('1', 'Album A', '1', 'klasický');
+INSERT INTO `albums` VALUES ('2', 'Beta', '1', 'klasický');
+INSERT INTO `albums` VALUES ('3', 'Common problems', '2', 'klasický');
+
+-- ----------------------------
+-- Table structure for `artists`
+-- ----------------------------
+DROP TABLE IF EXISTS `artists`;
+CREATE TABLE `artists` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+
+-- ----------------------------
+-- Records of artists
+-- ----------------------------
+INSERT INTO `artists` VALUES ('1', 'Cally McDown');
+INSERT INTO `artists` VALUES ('2', 'Peter File');
